e972f08b7210f0a4f02648f4fe1b920184df7f97
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2022-11-26 20:50:28 +0100

    Add parsing of the file name

--- libs/src/catasto/catasto/parser.py
+++ libs/src/catasto/catasto/parser.py
@@ -32,15 +32,28 @@ class FileParserService(FileParser):
             filetype = _reader.filetype.upper()
             filename = _reader.filename.upper()
             content = _reader.content
-            breakpoint()
+            name = filename.replace(filetype, "")
             # parsing carto reader content
-            if filetype in ["CXF", "CTF", "SUP"]:
+            carto = LandSheet()
+            if filetype in [".CXF", ".CTF", ".SUP"]:
                 # parse filename
+                carto = self._parse_name(carto, name)
                 # parse content
                 _iter = iter(content.splitlines())
                 for item in _iter:
-                    result = None
+                    # breakpoint()
+                    result = carto
                     # pass
             elif filetype in ["FAB", "TER", "SOG"]:
                 result = Census(codice_comune="H501")
-            yield result
+            return result
+
+    def _parse_name(self, land_sheet: LandSheet, name: str) -> LandSheet:
+        land_sheet.codice_foglio = name
+        land_sheet.codice_comune = name[:4]
+        land_sheet.codice_sezione_censuaria = name[4]
+        land_sheet.codice_numero_foglio = name[5:9]
+        land_sheet.numero_foglio = name[5:9].lstrip("0")
+        land_sheet.codice_allegato = name[9]
+        land_sheet.codice_sviluppo = name[10]
+        return land_sheet
