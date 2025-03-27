2caa790094bb37f444188b86a6409d7ab765b24e
Antonio Cerciello <antonio.cerciello@gmail.com>
2025-03-27 11:18:29 +0100

    Fix url

--- libs/src/catasto/catasto/duckdb/to_notify.py
+++ libs/src/catasto/catasto/duckdb/to_notify.py
@@ -204,7 +204,7 @@ def main():
     parser = argparse.ArgumentParser(description="Test loader function")
 
     parser.add_argument("--duckdb_filepath", default="/tmp/catasto.duckdb", help="Percorso file DuckDB")
-    parser.add_argument("--pg_conn_string", default="postgresql://siscatsrv:siscatsrv@localhost:5433/siscat", help="Connessione PostgreSQL")
+    parser.add_argument("--pg_conn_string", default="postgresql://catasto:catasto@localhost:5433/catasto", help="Connessione PostgreSQL")
     parser.add_argument("--pg_immobili_di_interesse_query", default="SELECT tit.codice as codice_comune, tit.immobile as codice_immobile, tit.tipo_imm as tipo_immobile FROM ctcn.titolarita_big_city tit GROUP BY tit.codice, tit.immobile, tit.tipo_imm", help="Query immobili di interesse")
 
     args = parser.parse_args()
