e411b4f8b3ade39ad13969feba4fb527a3016ed7
Francesco Bartoli <xbartolone@gmail.com>
2025-07-17 01:28:50 +0200

    Fix minio path and empty file list condition

--- flows/smidt_wf.py
+++ flows/smidt_wf.py
@@ -43,8 +43,8 @@ def smidt_flow():
     logger.info("Running smidt flow")
     files = observe_and_copy()
     logger.info(f"Files found in the smidt server: {files}")
+    cadaster_files = []
     if files:
-        cadaster_files = []
         for file in files:
             cadaster_files += process_smidt_file_flow(
                 minio_server=smidt_block.value["minio_host"],
@@ -52,41 +52,42 @@ def smidt_flow():
                 minio_secret_key=smidt_block.value["minio_secret_key"],
                 input_bucket=smidt_block.value["minio_bucket"],
                 p12_key=smidt_block.value["p12_certificate"],
-                enc_file_key=file.filename,
+                enc_file_key=file.minio_path,
                 ca_cert_key=smidt_block.value["CA_certificate"],
                 p12_password=smidt_block.value["p12_password"],
                 key_password=smidt_block.value["key_password"],
                 output_bucket=smidt_block.value["catasto_bucket"],
             )
-        return cadaster_files
     else:
         logger.info("There isn't any new file to process!")
+    return cadaster_files
 
 
 if __name__ == "__main__":
     files = smidt_flow()
-    local_sorted_files = download_and_sort_flow(
-        files_list=files,
-        minio_endpoint=smidt_block.value["minio_host"],
-        minio_access_key=smidt_block.value["minio_access_key"],
-        minio_secret_key=smidt_block.value["minio_secret_key"],
-        minio_bucket=smidt_block.value["catasto_bucket"],
-        secure=False,
-    )
-    ddb_file = ct2duckdb_flow(
-        fab_files=local_sorted_files["FAB"],
-        ter_files=local_sorted_files["TER"],
-        sog_files=local_sorted_files["SOG"],
-        tit_files=local_sorted_files["TIT"],
-        cxf_files=local_sorted_files["CXF"],
-        catasto_db=catastodb_block.value["duckdb_conn_string"],
-        empty_db=True,
-    )
-    ctcn_sync_flow(
-        source_db=ddb_file,
-        target_db=catastodb_block.value["pg_conn_string"],
-    )
-    ctmp_sync_flow(
-        source_db=ddb_file,
-        target_db=catastodb_block.value["pg_conn_string"],
-    )
+    if files:
+        local_sorted_files = download_and_sort_flow(
+            files_list=files,
+            minio_endpoint=smidt_block.value["minio_host"],
+            minio_access_key=smidt_block.value["minio_access_key"],
+            minio_secret_key=smidt_block.value["minio_secret_key"],
+            minio_bucket=smidt_block.value["catasto_bucket"],
+            secure=False,
+        )
+        ddb_file = ct2duckdb_flow(
+            fab_files=local_sorted_files["FAB"],
+            ter_files=local_sorted_files["TER"],
+            sog_files=local_sorted_files["SOG"],
+            tit_files=local_sorted_files["TIT"],
+            cxf_files=local_sorted_files["CXF"],
+            catasto_db=catastodb_block.value["duckdb_conn_string"],
+            empty_db=True,
+        )
+        ctcn_sync_flow(
+            source_db=ddb_file,
+            target_db=catastodb_block.value["pg_conn_string"],
+        )
+        ctmp_sync_flow(
+            source_db=ddb_file,
+            target_db=catastodb_block.value["pg_conn_string"],
+        )
